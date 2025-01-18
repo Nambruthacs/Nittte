adfa152eb2b97c5e4162e0e4d19520f5cee560da
Swastik Anchan <bangeraswastik2004@gmail.com>
2025-01-18 15:59:00 +0530

    contents are deleted

--- java_program/sum.java
+++ java_program/sum.java
@@ -3,8 +3,8 @@ class sum
    public static viod main(String args[])
   {
      int x,y sum;
-     x=5
-     y=6
+     x=5;
+     y=6;
      sum=x+y;
      system.out println("sum of two numbers is",sum);
   }
